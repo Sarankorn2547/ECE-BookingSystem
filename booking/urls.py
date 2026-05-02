3e6cfa6b1b1d23bd109963937103f4253cdab750
Ratchanon Moungwichean <mynameisnon23@gmail.com>
2026-05-02 20:05:08 +0700

    add my-bookings url

--- booking/urls.py
+++ booking/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("dashboard/", views.dashboard_view, name="dashboard"),
     path("booking/", views.booking_form_view, name="booking_form"),
     path("my-bookings/", views.my_bookings_view, name="my_bookings"),
+    path("my-bookings/<uuid:booking_id>/cancel/", views.cancel_booking_view, name="cancel_booking"),
     path("calendar/", views.calendar_view, name="calendar"),
 ]
